1bfca5e0d2bba8af8359b248c7b68c3c24b75cc5
divinechukwuma <divinechukwuma982@gmail.com>
2024-02-19 16:22:01 +0100

    finally added tailwind successfully

--- build/admin/index.php
+++ build/admin/index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Divine-Shop-Online</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    
+    <h1 class='text-5xl text-red-500'>tailwindcss is stressing me </h1>
 </body>
 </html>
