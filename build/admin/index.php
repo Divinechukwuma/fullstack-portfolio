2ccb5ee371005db8cf27452f762a3d14d29c17e0
divinechukwuma <divinechukwuma982@gmail.com>
2024-02-20 07:26:37 +0100

    learning css

--- build/admin/index.php
+++ build/admin/index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Divine-Shop-Online</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
-<body>
-    <h1 class='text-5xl text-red-500'>tailwindcss is stressing me </h1>
+
+<body class="min-h-screen">
+    <header class="bg-red-500 top-0 z-10 text-white sticky">
+        <section class=" max-w-8xl mx-auto p-4 flex justify-between items-center">
+            <h1 class="text-3xl font-semibold"> <a href="#">
+                    <span class="text-yellow-500">🎩 Divine-</span>Admin-Page</h1></a>
+            <button id="hamburger" class="text-3xl sm:block md:hidden cursor-pointer">
+                &#9776;
+            </button>
+            <nav class="hidden md:block space-x-3 text-xl">
+          <a href="#place" class="text-font-color hover:text-font-color-hover font-sans font-medium">Dashboard</a>
+          <a href="#place" class="text-font-color hover:text-font-color-hover font-sans font-medium">Admin</a>
+          <a href="#place" class="text-font-color hover:text-font-color-hover font-sans font-medium">Category</a>
+          <a href="#place" class="text-font-color hover:text-font-color-hover font-sans font-medium">Products</a>
+        </nav>
+        </section>
+    </header>
+    <main class="max-w-6xl"></main>
 </body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ['./build/*.php'],
   theme: {
-    extend: {},
-  },
+    colors:{
+      'unique-black':'#0D0D0D',
+      'font-color':'#F2F2F2',
+      'font-color-hover':'#D7D7D9',
+      'project-bg':'#A61420',
+      'project-bg-2':'#1DA1F2',
+      },
+
+   },
+    extend: {
+    },
+  
   plugins: [],
 }
 
