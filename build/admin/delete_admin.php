6c35b5c2f8c72eb79cc75f1f24d941e2e4e73024
divinechukwuma <divinechukwuma982@gmail.com>
2024-02-27 07:37:21 +0100

    finally fixed the delete bug

--- build/admin/delete_admin.php
+++ build/admin/delete_admin.php
@@ -1,44 +1,41 @@
-<?php 
-include('./config/database.php') ;
+<?php
+include('./config/database.php');
 
 //First get the id from the manage admin page
 
-if(isset($_GET['id'])){
+if (isset($_GET['id'])) {
 
-//Then confirm the id
+    //Then confirm the id
 
-$id = $_GET['id'];
+    $id = $_GET['id'];
 
-//The the sql query to delete from databse
+    //The the sql query to delete from databse
 
-$sql = "DELETE  FROM tbl_admin WHERE id=?";
+    $sql = "DELETE  FROM tbl_admin WHERE id=?";
 
-$stmt = $conn->prepare($sql);
-$stmt->bind_param('i',$id);
-$stmt->execute();
-$res = $stmt->get_result();
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param('i', $id);
+    $stmt->execute();
 
-if ($res===true) {
-    //data inserted
-    //create a session variabe to display message 
-    $_SESSION['del'] = "<div class='text-green-500'>Admin Deleted successfully.</div>";
 
-    //redirect page to manage admin
-    header("location: manage-admin.php"); //use a SITEURL constant 
+    if ($stmt->affected_rows > 0) {
+        //data inserted
+        //create a session variabe to display message 
+        $_SESSION['del'] = "<div class='text-green-500'>Admin Deleted successfully.</div>";
 
-} else {
-    //failed to insert data
-    //create a session variabe to display message 
-    $_SESSION['delete'] = "<div class='text-red-500'>Failed To Delete admin.</div>";
+        //redirect page to manage admin
+        header("location: manage-admin.php"); //use a SITEURL constant 
 
-    //redirect page to add admin
-    header("location: manage-admin.php"); //use a SITEURL constant 
+    } else {
+        //failed to insert data
+        //create a session variabe to display message 
+        $_SESSION['delete'] = "<div class='text-red-500'>Failed To Delete admin.</div>";
 
-}
+        //redirect page to add admin
+        header("location: manage-admin.php"); //use a SITEURL constant 
 
+    }
 }
 
 $stmt->close();
 $conn->close();
-
-?>
--- build/admin/manage-products.php
+++ build/admin/manage-products.php
@@ -8,89 +8,107 @@ if (isset($_SESSION['add'])) {
 
 ?>
 
+<?php
+if (isset($_SESSION['delete'])) {
+    echo $_SESSION['delete'];
+    unset($_SESSION['delete']);
+}
+
+?>
+
+<?php
+if (isset($_SESSION['del'])) {
+    echo $_SESSION['del'];
+    unset($_SESSION['del']);
+}
+
+?>
+
 
 <div class="my-10">
     <a class="  p-6  rounded-xl  bg-project-bg-2 hover:bg-blue-900 text-font-color m-10" href="add-products.php"> Add Products</a>
 
 
     <div class="m-10 pb-5">
-        <table>
-            <thead>
-                <tr class="text-4xl">
-                    <th class="p-6">S.N</th>
-                    <th class="p-6">Title</th>
-                    <th class="p-6">Discription</th>
-                    <th class="p-6">Prices</th>
-                    <th class="p-6">Image-Name</th>
-                    <th class="p-6">Feature</th>
-                    <th class="p-6">Active</th>
-                    <th class="p-6">Actions</th>
-                </tr>
-            </thead><br>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <table>
+                <thead>
+                    <tr class="text-4xl">
+                        <th class="p-6">S.N</th>
+                        <th class="p-6">Title</th>
+                        <th class="p-6">Discription</th>
+                        <th class="p-6">Prices</th>
+                        <th class="p-6">Image-Name</th>
+                        <th class="p-6">Feature</th>
+                        <th class="p-6">Active</th>
+                        <th class="p-6">Actions</th>
+                    </tr>
+                </thead><br>
 
-            <?php
+                <?php
 
-            //sql query to get data
-            $sql = "SELECT * FROM tbl_products";
-            $stmt = $conn->prepare($sql);
-            $stmt->execute();
-            $res = $stmt->get_result();
-            $sn = 1;
+                //sql query to get data
+                $sql = "SELECT * FROM tbl_products";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $res = $stmt->get_result();
+                $sn = 1;
 
-            //fetch data from database
+                //fetch data from database
 
-            if ($res->num_rows > 0) {
-                while ($row = $res->fetch_assoc()) {
-                    $id = $row['id'];
-                    $title = $row['title'];
-                    $description = $row['description'];
-                    $price = $row['price'];
-                    $imageName = $row['imageName'];
-                    $featured = $row['featured'];
-                    $active = $row['active'];
+                if ($res->num_rows > 0) {
+                    while ($row = $res->fetch_assoc()) {
+                        $id = $row['id'];
+                        $title = $row['title'];
+                        $description = $row['description'];
+                        $price = $row['price'];
+                        $imageName = $row['imageName'];
+                        $featured = $row['featured'];
+                        $active = $row['active'];
 
-            ?>
-
-
-                    <tbody class="text-xl border-t-0 border-gray-300">
-                        <tr class="text-xl ">
-                            <td class="p-6 border"><?php echo htmlspecialchars($sn++) ?></td>
-                            <td class="p-6 border"><?php echo htmlspecialchars($title) ?></td>
-                            <td class="p-6 border "><?php echo htmlspecialchars($description) ?></td>
-                            <td class="p-6 border ">$<?php echo htmlspecialchars($price) ?></td>
-                            <td class="p-6 border ">
-                                <?php
-                                // Check whether we have an image or not 
-                                if ($imageName == "") {
-                                    // We do not have an image. Display an error message
-                                    echo "<div class='text-red-500 uppercase'> Image not added.</div>";
-                                } else {
-                                    // We have an image, display the image 
-                                ?>
-                                    <img src="./images/goods<?php echo htmlspecialchars($imageName); ?> " width="150px">
-                                <?php
-                                }
-                                ?>
-                            </td>
-                            <td class="p-6 border "><?php echo htmlspecialchars($featured) ?></td>
-                            <td class="p-6 border "><?php echo htmlspecialchars($active) ?></td>
-                            <td class=" p-6 border"><a class="  p-5  rounded-xl  bg-project-bg-2 hover:bg-blue-900 text-font-color" href="update-products.php?id=<?php echo $id; ?>"> Update Products</a></td>
-                            <td class="p-2 border"><a class="  p-5  rounded-xl  bg-red-300 hover:bg-red-600 text-font-color" href="delete-products.php?id=<?php echo $id ?>"> Delete Products</a></td>
-                        </tr>
-                    </tbody>
+                ?>
 
 
+                        <tbody class="text-xl border-t-0 border-gray-300">
+                            <tr class="text-xl ">
+                                <td class="p-6 border"><?php echo htmlspecialchars($sn++) ?></td>
+                                <td class="p-6 border"><?php echo htmlspecialchars($title) ?></td>
+                                <td class="p-6 border "><?php echo htmlspecialchars($description) ?></td>
+                                <td class="p-6 border ">$<?php echo htmlspecialchars($price) ?></td>
+                                <td class="p-6 border ">
+                                    <?php
+                                    // Check whether we have an image or not 
+                                    if ($imageName == "") {
+                                        // We do not have an image. Display an error message
+                                        echo "<div class='text-red-500 uppercase'> Image not added.</div>";
+                                    } else {
+                                        // We have an image, display the image 
+                                    ?>
+                                        <img src="./images/goods<?php echo htmlspecialchars($imageName); ?> " width="150px">
+                                    <?php
+                                    }
+                                    ?>
+                                </td>
+                                <td class="p-6 border "><?php echo htmlspecialchars($featured) ?></td>
+                                <td class="p-6 border "><?php echo htmlspecialchars($active) ?></td>
+                                <td class=" p-6 border"><a class="  p-5  rounded-xl  bg-project-bg-2 hover:bg-blue-900 text-font-color" href="update-products.php?id=<?php echo $id; ?>"> Update Products</a></td>
+                                <td class="p-2 border"><a class="  p-5  rounded-xl  bg-red-300 hover:bg-red-600 text-font-color" href="delete-products.php?id=<?php echo $id ?>&imageName=<?php echo $imageName ?>"> Delete Products</a></td>
+                            </tr>
+                        </tbody>
 
-            <?php
 
+
+                <?php
+
+                    }
                 }
-            }
 
 
 
-            ?>
+                ?>
 
-        </table>
+            </table>
+        </form>
 
     </div>
 
