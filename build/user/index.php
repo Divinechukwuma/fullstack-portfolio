7225c8f4d6c0f74558b68e8140ffca9f51be9f50
divinechukwuma <divinechukwuma982@gmail.com>
2024-02-29 08:26:08 +0100

    im awesome

--- build/user/index.php
+++ build/user/index.php
@@ -74,7 +74,7 @@
   </header>
   <section class="w-[80%] mt-10" id="products">
 
-    <div class="bg-unique-black ml-96">
+    <div class="bg-unique-black md:ml-96">
       <div class="font-sans text-center text-black p-5">
         <input class=" bg-white p-3 rounded pl-10 w-[60%]" type="text" placeholder="Search For Products">
       <button class="bg-project-bg-2 p-3 rounded ml-2 text-white">Search</button>
@@ -145,11 +145,12 @@
   <!--- || PRODCUTS-->
   <section class="w-[80%] " id="products">
 
-    <div class="bg-project-bg-2 ml-96">
+    <div class="bg-project-bg-2 md:ml-96 ">
       <h1 class="font-sans text-3xl font-bold text-center text-white p-4">Buy And Order</h1>
     </div>
 
   </section>
+  
 </body>
 
 </html>
