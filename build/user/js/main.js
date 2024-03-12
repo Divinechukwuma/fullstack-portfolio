f2f089928d442db3f1759ccdea2a8a6b9c7b00bf
divinechukwuma <divinechukwuma982@gmail.com>
2024-03-12 09:38:56 +0100

    im tired

---
+++ build/user/js/main.js
@@ -0,0 +1,15 @@
+const initApp =() => {
+    const hamburgerBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+    //    // the added js file for the second variation for the hamburger 
+    //     hamburgerBtn.classList.toggle('toggle-btn')
+    }
+    hamburgerBtn.addEventListener('click',toggleMenu)
+    mobileMenu.addEventListener('click',toggleMenu)
+}
+
+document.addEventListener('DOMContentLoaded',initApp)
--- build/user/partials/header.php
+++ build/user/partials/header.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js" integrity="sha512-HGOnQO9+SP1V92SrtZfjqxxtLmVzqZpjFFekvzZVWoiASSQgSr4cw9Kqd2+l8Llp4Gm0G8GIFJ4ddwZilcdb8A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="../js/main.js" defer></script>
 
   <!-- <script src="./javascript/slider.js" defer></script> -->
 </head>
