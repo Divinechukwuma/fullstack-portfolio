088bd76529b47a67ee2ee6d248551e8fc5f3ebd6
divinechukwuma <divinechukwuma982@gmail.com>
2024-03-16 13:08:18 +0100

    moving fast

--- build/user/order.php
+++ build/user/order.php
@@ -1,6 +1,30 @@
 <?php include('./partials/header.php') ?>
 <div id="order">
-    
+
+<?php if(isset($_GET['orderId'])){
+     
+     $orderId = $_GET['orderId'];
+    //  echo "im too good";
+     //sql query to get the data from the product
+
+     $sql = "SELECT * FROM tbl_products WHERE id=?";
+     $stmt = $conn->prepare($sql);
+     $stmt->bind_param('d',$orderId);
+     $stmt->execute();
+     $res = $stmt->get_result();
+
+     if($res->num_rows > 0){
+
+        // echo "im too  good";
+
+        
+
+     }
+
+
+
+} ?>
+
 </div>
 
 
