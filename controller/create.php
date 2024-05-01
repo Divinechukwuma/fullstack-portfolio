3ca6fb8e46ef23344964f652c7e457b9ff2473a5
divinechukwuma <divinechukwuma982@gmail.com>
2024-05-01 18:26:14 +0100

    i will figure it out

---
+++ controller/create.php
@@ -0,0 +1,32 @@
+<?php
+
+// Sql query to fetch data from database
+
+$sql = "SELECT * FROM tbl_category LIMIT 6";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$res = $stmt->get_result();
+
+if ($res->num_rows > 0) {
+  while ($row = $res->fetch_assoc()) {
+    $id = $row['id'];
+    $title = $row['title'];
+    $imageName = $row['imageName'];
+  }}
+
+  
+      //Sql to get the data from the database
+      $sql = "SELECT * FROM tbl_products ORDER BY imageName DESC";
+      $stmt = $conn->prepare($sql);
+      $stmt->execute();
+      $res = $stmt->get_result();
+
+      if ($res->num_rows > 0) {
+        while ($row = $res->fetch_assoc()) {
+          $id = $row['id'];
+          $title = $row['title'];
+          $description = $row['description'];
+          $price = $row['price'];
+          $imageName = $row['imageName']; // Fixed variable name
+          $categoryId = $row['categoryId'];
+        }}
--- core/router.php
+++ core/router.php
@@ -1,7 +1,53 @@
 <?php
+
 namespace core;
 
-class Router    
+class Router
 {
 
+    protected  $routes = [];
+
+    public function add($method, $uri, $controller)
+    {
+
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' =>  $controller,
+            'method' => $method
+        ];
+
+        return $this;
+    }
+
+    public function get($uri, $controller)
+    {
+        return $this->add('GET', $uri, $controller);
+    }
+
+    public function post($uri, $controller)
+    {
+        return $this->add('POST', $uri, $controller);
+    }
+
+    public function delete($uri, $controller)
+    {
+        return $this->add('DELETE', $uri, $controller);
+    }
+
+    public function patch($uri, $controller)
+    {
+        return $this->add('PATCH', $uri, $controller);
+    }
+
+    public function put($uri, $controller)
+    {
+        return $this->add('PUT', $uri, $controller);
+    }
+
+    protected function abort($code = 404) {
+        http_response_code($code);
+        require 'controllers/404.php';
+
+        die();
+    }
 }
