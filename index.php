fbd3bb030051121205e118d633c166ae003bb301
divinechukwuma <divinechukwuma982@gmail.com>
2024-05-02 09:11:03 +0100

    let figure this out

--- index.php
+++ index.php
@@ -13,3 +13,14 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
 
 $router->route($uri, $method);
+
+
+function dd($routes)
+{
+    echo "<pre>";
+
+    var_dump($routes);
+
+    echo "</pre>";
+    die();
+}
